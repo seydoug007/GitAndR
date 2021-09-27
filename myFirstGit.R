64e9e20dd41341c81cffce041fa235daac94c973
Seydou GORE <seydoug007@gmail.com>
2021-09-27 15:11:36 +0200

    Change I will later undo

---
+++ myFirstGit.R
@@ -0,0 +1,6 @@
+
+Names<-c("Seydou","Marwa","Marc","Laitissia","Fouad","Sabrina","Wendy-ann")
+Positions<-c("Data analyst","Biostatistician","CEO","Biostatistician","Project chief",NA,"Monitor")
+Gender<-c("Male","Female","Male","Female","Male","Female","Female")
+mydata<-data.frame(Names, Positions)
+
